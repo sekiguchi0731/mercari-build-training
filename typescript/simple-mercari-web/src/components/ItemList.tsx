f28cace36abc267ec52fce87ed42a25d2ab75a1a
sekiguchi0731 <seki.guchi.3725@gmail.com>
2025-03-12 19:28:35 +0900

    [update]change the front like marcari(step9-4)

--- typescript/simple-mercari-web/src/components/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList.tsx
@@ -29,7 +29,7 @@ export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
   }, [reload, onLoadCompleted]);
 
   return (
-    <div>
+    <div className='ItemField'>
       {items?.map((item) => {
         return (
           <div key={item.id} className="ItemList">
